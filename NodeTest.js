e5c799d0f19c980b96d4a258cda991a992b3bf17
pragyavarshney <varshney.pragya@gmail.com>
2019-11-22 16:29:45 -0800

    Commit1

--- NodeTest.js
+++ NodeTest.js
@@ -1,23 +1,91 @@
+const {format} = require('util');
 const express = require('express');
+const multer = require('multer');
+
 const app = express();
+const upload = multer();
+
 const vision = require('@google-cloud/vision');
-// Creates a client
-const client = new vision.ImageAnnotatorClient({
-    keyFilename: 'Api-key.json'
-});
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
-// Performs label detection on the image file
-client
-    .labelDetection('./testimage.jpg')
-    .then(results => {
-        const labels = results[0].labelAnnotations;
 
-        console.log('Labels:');
-        labels.forEach(label => console.log(label));
-        //console.log(results);
-    })
-    .catch(err => {
-        console.error('ERROR:', err);
+/*app.use(express.static(path.join(__dirname, 'views')));*/
+app.set('view engine', 'ejs');
+
+async function analyze(req, res) {
+  // Imports the Google Cloud client library
+
+  // Creates a client
+    const client = new vision.ImageAnnotatorClient({
+        keyFilename: 'Api-key.json'
     });
 
-app.listen(5000, '127.0.0.1', () => console.log('Server running'));
+  if ( req.file ) {
+
+      const blob = bucket.file(req.file.originalname);
+      const blobStream = blob.createWriteStream({
+          resumable: false,
+      });
+
+      blobStream.on('error', err => {
+          next(err);
+      });
+
+      let publicUrl = '';
+      blobStream.on('finish', () => {
+          // The public URL can be used to directly access the file via HTTP.
+           publicUrl = format(
+              `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+          );
+      });
+
+      blobStream.end(req.file.buffer);
+
+      // Performs label detection on the image file
+      client
+          .labelDetection('gs://my-project-image-258819.appspot.com/' + req.file.originalname)
+          .then(results => {
+              console.log("ANALYZING IMAGE");
+              const labels = results[0].labelAnnotations;
+
+              console.log('Labels:');
+              labels.forEach(label => console.log(label.description));
+              //console.log(results);
+          });
+
+      res.render('ImageInsert', {
+          labels: labels,
+      });
+  }
+}
+
+app.get('/', function(req, res) {
+    let labels = null;
+    res.render('ImageInsert', {
+        labels: labels,
+    });
+});
+
+app.get('/HowTo', function(req, res) {
+    let labels = null;
+    res.render('HowTo');
+});
+
+app.post('/upload', upload.single('pic'), function(req, res) {
+    let labels = null;
+    if ( req.file ) {
+        analyze(req, res).then(r => {});
+        return;
+    }
+    res.render('ImageInsert', {
+        labels: labels,
+    });
+
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`LISTENING ON PORT ${PORT}`);
+});
